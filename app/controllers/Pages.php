008ffc5a309537ae02b51b2bafbe64fd621c639a
Iulian Oana <iulian.andreioana@gmail.com>
2020-05-02 12:26:50 +0100

    Add page view layout

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,6 +7,7 @@ class Pages extends Controller {
     $this->itemModel = $this->model('Item');
   }
 
+  // Functionality for the Dashboard page
   public function dashboard(){
     // Redirect if not logged in.
     if(!isLoggedIn()) redirect('pages/index');
@@ -18,7 +19,33 @@ class Pages extends Controller {
       'email' => $_SESSION['email'],
       'registeredOn' => $_SESSION['registeredOn']
     ];
+    // Load the dashboard.
     $this->view('pages/dashboard', $data);
+  }
+
+  public function add() {
+    // Redirect if not logged in.
+    if(!isLoggedIn()) redirect('pages/index');
+
+    // Load the add view.
+    $this->view('pages/add');
+  }
+
+  // Functionality for the Profile page.
+  public function profile(){
+    // Redirect if not logged in.
+    if(!isLoggedIn()) redirect('pages/index');
+
+    $data = [
+      'firstName' => $_SESSION['firstName'],
+      'lastName' => $_SESSION['lastName'],
+      'fullName' => $_SESSION['fullName'],
+      'email' => $_SESSION['email'],
+      'registeredOn' => $_SESSION['registeredOn']
+    ];
+
+    // Load the profile view.
+    $this->view('pages/profile', $data);
   }
 
   public function index(){
